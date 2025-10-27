311bc8f62152cbc8ab0390dc876df544c27b326c
leonardo-ga <leonardo.r.m.gasparro@gmail.com>
2025-10-27 15:01:55 +0100

    added max/min distances from cam to car

--- src/js/scene/Camera.js
+++ src/js/scene/Camera.js
@@ -38,6 +38,8 @@ export default class Camera {
     this.controls = new OrbitControls(this.instance, this.base.canvas);
     this.controls.enableDamping = true;
     this.controls.dampingFactor = 0.1;
+    this.controls.minDistance = 5;
+    this.controls.maxDistance = 30;
   }
 
   resize() {
